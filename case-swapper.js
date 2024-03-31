692598d550fea8cbcc3152df1b8fd2160e1a7d72
m-murigi <162122689+m-murigi@users.noreply.github.com>
2024-04-01 00:26:56 +0300

    Update case-swapper.js

--- case-swapper.js
+++ case-swapper.js
@@ -1,5 +1,5 @@
    
-    
+      //input string  
 let string ="The Quick Brown Fox";
 
 //'tHE qUICK bROWN fOX'
@@ -7,6 +7,7 @@ function swapCase(string) {
     const words = string.split('  ');
 
     return words.map(word => {
+       // Change the first character to lowercase and the rest of the characters to uppercase
         return word[0].toLowerCase() + word.slice(1).toUpperCase();
     }).join(" ");
 }
