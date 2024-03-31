f9d71700638a4df54c50a4b1c89511b5e7944ad2
m-murigi <162122689+m-murigi@users.noreply.github.com>
2024-04-01 00:25:14 +0300

    Update array-generator.js

--- array-generator.js
+++ array-generator.js
@@ -1,8 +1,10 @@
 function generateAnArray(num1,num2){
+     //an empty array that stores the generated numbers
 let arr =[];
 for(let i=num1;i<=num2;i++){
     arr.push(i);
 }
+    //return generated array
 return arr;
 }
 console.log(generateAnArray (-4,7))
